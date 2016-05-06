d97a2e1803d909d34adb5fd81890099b0bd751ff
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-05-06 09:17:16 -0500

    added readme

--- public/js/app.js
+++ public/js/app.js
@@ -5,41 +5,41 @@ var socket = io();
 console.log(name + ' wants to join ' + room);
 
 // Update h1 tag
-jQuery('.room-title').text(room);
+$('.room-title').text(room);
 
-socket.on('connect', function () {
-	console.log('Conncted to socket.io server!');
-	socket.emit('joinRoom', {
-		name: name,
-		room: room
-	});
+socket.on('connect', function() {
+    console.log('Conncted to socket.io server!');
+    socket.emit('joinRoom', {
+        name: name,
+        room: room
+    });
 });
 
-socket.on('message', function (message) {
-	var momentTimestamp = moment.utc(message.timestamp);
-	var $messages = jQuery('.messages');
-	var $message = jQuery('<li class="list-group-item"></li>');
+socket.on('message', function(message) {
+    var momentTimestamp = moment.utc(message.timestamp);
+    var $messages = $('.messages');
+    var $message = $('<li class="list-group-item"></li>');
 
-	console.log('New message:');
-	console.log(message.text);
+    console.log('New message:');
+    console.log(message.text);
 
-	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
-	$message.append('<p>' + message.text + '</p>');
-	$messages.append($message);
+    $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm:ss a') + '</strong></p>');
+    $message.append('<p>' + message.text + '</p>');
+    $messages.append($message);
 });
 
 // Handles submitting of new message
-var $form = jQuery('#message-form');
+var $form = $('#message-form');
 
-$form.on('submit', function (event) {
-	event.preventDefault();
+$form.on('submit', function(event) {
+    event.preventDefault();
 
-	var $message = $form.find('input[name=message]');
+    var $message = $form.find('input[name=message]');
 
-	socket.emit('message', {
-		name: name,
-		text: $message.val()
-	});
+    socket.emit('message', {
+        name: name,
+        text: $message.val()
+    });
 
-	$message.val('');
+    $message.val('');
 });
